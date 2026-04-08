6629a1ee7a6205238a36439b18475d56a05e9fb0
Daniil Iaitskov <dyaitskov@gmail.com>
2026-04-08 20:26:25 +0300

    test eToView with non-empty-eacs

--- test/Miso/Css/Test/Style.hs
+++ test/Miso/Css/Test/Style.hs
@@ -6,7 +6,7 @@ module Miso.Css.Test.Style where
 import Data.Proxy ( Proxy(Proxy) )
 import Data.ByteString.Lazy qualified as L
 import Data.ByteString.Char8 qualified as C8
-import Miso.Css.Style ( eToView, E(..), OrClass(TopOrClass) )
+import Miso.Css.Style
 import Miso.Css.Prelude
 import Miso.Html ( div_, ToHtml(toHtml) )
 import Test.Tasty ( testGroup, TestTree )
@@ -18,25 +18,66 @@ test_style =
   [ testCase "div_" do
       toHtml (div_ [] []) @?= "<div></div>"
   , testGroup "eToView"
-    [ "cdata hello" `go` CDataE "cdata hello"
-    , "<div></div>" `go` NilE (Proxy @"div")
-    , """<div class="c"></div>""" `go` AppClsE (TopOrClass (Proxy @"c")) (NilE (Proxy @"div"))
-    , """<div class="c">cd</div>""" `go`
-      AppendChildE
-        (CDataE "cd")
-        (AppClsE (TopOrClass (Proxy @"c"))
+    [ testGroup "empty eacs"
+      [ "cdata hello" `go` CDataE "cdata hello"
+      , "<div></div>" `go` NilE (Proxy @"div")
+      , """<div class="c"></div>""" `go`
+        AppClsE (TopOrClass (Proxy @"c")) (NilE (Proxy @"div"))
+      , """<div class="c">cd</div>""" `go`
+        AppendChildE
+          (CDataE "cd")
+          (AppClsE (TopOrClass (Proxy @"c"))
+            (NilE (Proxy @"div")))
+      , """<div class="c"><br/></div>""" `go`
+        AppendChildE
+          (NilE $ Proxy @"br")
+          (AppClsE (TopOrClass (Proxy @"c"))
+            (NilE (Proxy @"div")))
+      , """<div class="a"><div class="b"></div></div>""" `go`
+        AppendChildE
+          (AppClsE (TopOrClass (Proxy @"b"))
+            (NilE $ Proxy @"div"))
+          (AppClsE (TopOrClass (Proxy @"a"))
+            (NilE (Proxy @"div")))
+      ]
+    , testGroup "non-empty-eacs"
+      [ """<div class="a"><div class="b"></div></div>""" `go`
+        AppendChildE
+        (AppClsE
+          (AddAncestorBranch
+            (AddAncestor (Proxy @"a") CssOrphan)
+            (TopOrClass (Proxy @"b")))
           (NilE (Proxy @"div")))
-    , """<div class="c"><br/></div>""" `go`
-      AppendChildE
-        (NilE $ Proxy @"br")
-        (AppClsE (TopOrClass (Proxy @"c"))
-          (NilE (Proxy @"div")))
-    , """<div class="a"><div class="b"></div></div>""" `go`
-      AppendChildE
-        (AppClsE (TopOrClass (Proxy @"b"))
-          (NilE $ Proxy @"div"))
         (AppClsE (TopOrClass (Proxy @"a"))
           (NilE (Proxy @"div")))
+      , """<div class="a"><div class="b"><div class="c"></div></div></div>""" `go`
+        AppendChildE
+        (AppendChildE
+         (AppClsE
+           (AddAncestorBranch
+             (AddAncestor (Proxy @"b") (AddAncestor (Proxy @"a") CssOrphan))
+             (TopOrClass (Proxy @"c")))
+           (NilE (Proxy @"div")))
+         (AppClsE (TopOrClass (Proxy @"b"))
+           (NilE (Proxy @"div"))))
+        (AppClsE (TopOrClass (Proxy @"a"))
+         (NilE (Proxy @"div")))
+      , """<div class="a"><div class="b"><div class="c"></div></div></div>""" `go`
+        AppendChildE
+        (AppendChildE
+         (AppClsE
+           (AddAncestorBranch
+             (AddAncestor (Proxy @"b") (AddAncestor (Proxy @"a") CssOrphan))
+             (TopOrClass (Proxy @"c")))
+           (NilE (Proxy @"div")))
+         (AppClsE
+          (AddAncestorBranch
+           (AddAncestor (Proxy @"a") CssOrphan)
+            (TopOrClass (Proxy @"b")))
+           (NilE (Proxy @"div"))))
+        (AppClsE (TopOrClass (Proxy @"a"))
+         (NilE (Proxy @"div")))
+      ]
     ]
   ]
   where
